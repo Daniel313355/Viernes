ab74902f93c93766b52e70440d5f224e86f758a0
Daniel313355 <daniel.vieda@uniagustiniana.edu.co>
2025-04-05 01:16:44 +0000

    r

--- js/api.js
+++ js/api.js
@@ -1,10 +1,10 @@
-async function  conexionLista() {
+async function  conexionlista() {
     const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1025')
     const data = await res.json()
     return data.results;
 }
 
 async function General() {
-    const infoPokes = await conexionLista();
-    mostrarLista(infoPokes)
+    const infoPokes = await conexionlista();
+    mostrarlista(infoPokes)
 }
--- js/lista.js
+++ js/lista.js
@@ -1,4 +1,9 @@
-function mostrarlista(){
+function mostrarlista(listaPokes){
     const app = document.getElementById("app");
+    let Pokes ="";
+
+    for (let i = 0; i < listaPokes.length; i++) {
+        Pokes += listaPokes[i].name
+    }
+
     app.innerHTML = "lista";
-}
